348cdb46d7617035e356d68b7ed9a7620797bba2
tj <tj@dcsnewyork.com>
2016-11-21 00:30:36 -0500

    fixed bug that was double loading jquery

--- page_search.php
+++ page_search.php
@@ -173,7 +173,6 @@ $subjects = $sub->fetchAll();
             <hr class="clear" />
         </footer>
 
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <script>
                     $("#selectSubject").select2();
                     $("#selectInstruct").select2();
